472a5aa619c6d8d20d9530d437b8f8704a91f048
Favour <georgefavour694@gmail.com>
2025-10-16 11:59:20 +0100

    health-check

--- server.py
+++ server.py
@@ -1512,5 +1512,10 @@ def terms_of_service():
 def privacy_policy():
     return render_template("privacy_policy.html")
 
+
+@app.route('/health-check')
+def health_check():
+    return {"meesage": "healthy"}
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False, port=int(os.getenv("PORT", 5000))) 
